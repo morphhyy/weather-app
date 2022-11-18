15bf58f20c977c8d86901b043ff279df6ddcaa38
morphhyy <imbibek05@gmail.com>
2022-11-18 18:07:46 +0545

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const [city, setCity] = useState("");
+    const [info, setInfo] = useState({});
+    const imageurl =
+        city &&
+        `https://images.unsplash.com/photo-1553901753-215db344677a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80`;
+
+    const getData = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e6630a9c0a1bcd4777dc0a539b027e87`
+            );
+            const data = await response.json();
+            if (data.cod !== "404") {
+                setInfo({
+                    sky: data.weather[0].main,
+                    temp: data.main.temp,
+                });
+
+                setTimeout(() => {
+                    setInfo({});
+                    setCity("");
+                }, 8000);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <div
+            className="container"
+            style={{ backgroundImage: `url(${imageurl})` }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <form method="POST" onSubmit={getData}>
+                <input
+                    className="inputCity"
+                    type="text"
+                    name="city"
+                    placeholder="Enter city"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                />
+            </form>
+            {info.temp && (
+                <div className="tempinfo">
+                    <h1>{info.sky}</h1>
+                    <h2>{info.temp} °C</h2>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
